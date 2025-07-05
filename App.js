aff22bc5c821fb37fd89c01c8cde6aa75d58b2a4
OhLaLaTartine <grei7vetur@gmail.com>
2025-07-04 22:48:49 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ooohh La La Tartine</h1>
+        <p>Oui to Delicious! Oui to Ooohh La La Tartine.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
